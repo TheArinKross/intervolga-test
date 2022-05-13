c33faecf6c3c5f5235ff6b77d283ae446c7b651b
TheArinKross <a.strelnikow@gmail.com>
2022-05-13 16:05:23 +0300

    Создание index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Тест Стрельников А.Ю</title>
+</head>
+<body>
+    <h1 class="header">Тестовое задание Стрельников А.Ю.</h1>
+    <a href="task1/algoritm.php">Задание 1. Алгоритм</a><br>
+    <a href="task2/upload.php">Задание 2. Web</a><br>
+    <a href="task3-4/sql.php">Задание 3-4. SQL</a>
+</body>
+</html>
